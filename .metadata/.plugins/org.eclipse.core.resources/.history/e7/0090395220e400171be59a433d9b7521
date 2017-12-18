162d27dc40df05357099fc6d38845080dfc76cba
Rvphvel <raphaa.ld@gmail.com>
2017-12-18 16:24:15 -0200

    Criando operação de instaciação

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/e7/0090395220e400171be59a433d9b7521
@@ -0,0 +1,12 @@
+package br.com.springmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmcApplication.class, args);
+	}
+}
--- springmc/src/main/java/br/com/springmc/controller/CategoriaController.java
+++ springmc/src/main/java/br/com/springmc/controller/CategoriaController.java
@@ -1,28 +1,27 @@
 package br.com.springmc.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.springmc.domain.Categoria;
+import br.com.springmc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaController {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria categoria1 = new Categoria(1,"Informática");
-		Categoria categoria2 = new Categoria(2, "Escritório");
+		Categoria categoria = categoriaService.listaCategorio(id);
 		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(categoria1);
-		lista.add(categoria2);
-		
-		return lista;
+		return ResponseEntity.ok().body(categoria);
 	}
 }
